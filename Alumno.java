2d6ab67e0fd20d6f14d98755fb92e7299bdfc1f6
martindc <martindc@iesgalileo.es>
2023-04-12 19:37:00 +0200

    v1: * Alumno.java contructor * Programa.java vacio

---
+++ Alumno.java
@@ -0,0 +1,11 @@
+class Alumno
+{
+   //atributos
+   private int nm;
+   private String nombre;
+   //constructor
+   public Alumno(int nm, String nombre){
+      this.nm=nm;
+      this.nombre=nombre;
+   }
+}
